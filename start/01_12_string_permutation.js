5a3c30b0ae88549c624606979fc36b7efdd12eae
Elakya <elakya.velumaniyan@gmail.com>
2023-09-22 11:05:32 +0000

    [solve] 11 to 14 level up problems

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,15 @@
 // Write your code here
+function stringPermutation(value) {
+  if(value === "") {
+    return 0;
+  }
+  if(value.length === 1) {
+    return 1;
+  }
+  return value.length * stringPermutation(value.slice(1))
+}
+
+console.log(stringPermutation("hello"));
+console.log(stringPermutation(""));
+console.log(stringPermutation("h"));
+console.log(stringPermutation("hell"));
--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,17 @@
 // Write your code here
+function createUrlify(value) {
+  // const lowercaseValues = value.toLowerCase();
+  // const valueArray = lowercaseValues.split(' ');
+  // const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+  // const processedArray = valueArray.map(item => item.replace(specialChars, ""))
+  // return processedArray.join('-');
+
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+  const stringWithoutSpecialChars = value.replaceAll(specialChars, "");
+  return stringWithoutSpecialChars.toLowerCase().trim().replaceAll(' ', '-');
+}
+
+console.log(createUrlify('My Blog Name!'));
+console.log(createUrlify("Elakya's Blog"));
+console.log(createUrlify("How I got into Programming"));
+console.log(createUrlify("I've got a new job :)"));
