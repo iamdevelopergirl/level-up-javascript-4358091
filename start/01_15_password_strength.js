7e720196393e5bcbaf2750f1cbcf37b724a0f1e5
Elakya <elakya.velumaniyan@gmail.com>
2023-09-22 11:36:10 +0000

    [solve]final level up problem

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+function passwordStrengthCheck(password) {
+  const passwordPattern = /(?=.*[!@#$%^&*])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/g
+  return passwordPattern.test(password) ? 'Valid' : 'Not Valid';
+}
+
+console.log(passwordStrengthCheck("JackSparrow*4"))
+console.log(passwordStrengthCheck("jacksparrow*4"))
+console.log(passwordStrengthCheck("JackSparrow*"))
+console.log(passwordStrengthCheck("JackSparrow4"))
+console.log(passwordStrengthCheck("Jack*4"))
+
